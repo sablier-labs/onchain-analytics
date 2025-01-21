58e0691e6d291a31d43d96a522227563687d977c
maxdesalle <max@sablier.finance>
2025-01-21 11:06:57 -0500

    feat: add legacy stream creation count

---
+++ queries/legacy:_stream_creation_count___4600780.sql
@@ -0,0 +1,9 @@
+-- part of a query repo
+-- query name: Legacy: Stream Creation Count
+-- query link: https://dune.com/queries/4600780
+
+
+SELECT
+    COUNT(*)
+FROM
+    query_4596310 -- Unified: Legacy Stream Creation Data
