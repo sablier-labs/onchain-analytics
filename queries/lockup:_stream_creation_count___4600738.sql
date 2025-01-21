c93190b80f5668fda1fcd1b3703c2ba5eb96476e
maxdesalle <max@sablier.finance>
2025-01-21 11:03:26 -0500

    feat: add lockup stream creation count

---
+++ queries/lockup:_stream_creation_count___4600738.sql
@@ -0,0 +1,9 @@
+-- part of a query repo
+-- query name: Lockup: Stream Creation Count
+-- query link: https://dune.com/queries/4600738
+
+
+SELECT
+    COUNT(*)
+FROM
+    query_4580489 -- Unified: Lockup Stream Creation Data
