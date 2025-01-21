4cd1b096ef8f74eefd5b3b60e722c0dc3bd58dec
maxdesalle <max@sablier.finance>
2025-01-21 11:09:55 -0500

    feat: add flow stream creation count

---
+++ queries/flow:_stream_creation_count___4600798.sql
@@ -0,0 +1,9 @@
+-- part of a query repo
+-- query name: Flow: Stream Creation Count
+-- query link: https://dune.com/queries/4600798
+
+
+SELECT
+    COUNT(*)
+FROM
+    query_4596391 -- Unified: Flow Stream Creation Data
