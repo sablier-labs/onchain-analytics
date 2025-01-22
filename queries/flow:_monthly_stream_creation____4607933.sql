af9d200bb9c04b4be4986bd31c438982763270f2
maxdesalle <max@sablier.finance>
2025-01-22 17:37:37 -0500

    feat: add flow stream creation count change

---
+++ queries/flow:_monthly_stream_creation____4607933.sql
@@ -0,0 +1,19 @@
+-- part of a query repo
+-- query name: Flow: Monthly Stream Creation Count Change
+-- query link: https://dune.com/queries/4607933
+
+
+SELECT
+    CASE
+        WHEN previous_month_streams = 0 THEN NULL
+        ELSE ((current_month_streams - previous_month_streams) * 100.0 / previous_month_streams)
+    END AS percentage_change
+FROM (
+    SELECT
+        SUM(CASE WHEN evt_block_time >= CURRENT_DATE - INTERVAL '30' day THEN 1 ELSE 0 END) AS current_month_streams,
+        SUM(CASE WHEN evt_block_time >= CURRENT_DATE - INTERVAL '60' day AND evt_block_time < CURRENT_DATE - INTERVAL '30' day THEN 1 ELSE 0 END) AS previous_month_streams
+    FROM
+        query_4596391 -- Flow: Stream Creation Data
+    WHERE
+        evt_block_time >= CURRENT_DATE - INTERVAL '60' day
+) AS streams_data;
