35d06996ab36c325a6df1427209d43d4bdb89704
maxdesalle <max@sablier.finance>
2025-02-03 15:40:29 -0500

    feat: add withdrawal data query

---
+++ queries/lockup:_withdrawal_data___4611102.sql
@@ -0,0 +1,59 @@
+-- part of a query repo
+-- query name: Lockup: Withdrawal Data
+-- query link: https://dune.com/queries/4611102
+
+
+SELECT
+    chain,
+    'lockupLinear' AS contract,
+    evt_block_time,
+    streamid,
+    asset AS token,
+    amount,
+    'v1.1' AS release_version
+FROM
+    sablier_v2_1_multichain.sablierv2lockuplinear_evt_withdrawfromlockupstream
+UNION ALL
+SELECT
+    chain,
+    'lockupDynamic' AS contract,
+    evt_block_time,
+    streamid,
+    asset AS token,
+    amount,
+    'v1.1' AS release_version
+FROM
+    sablier_v2_1_multichain.sablierv2lockupdynamic_evt_withdrawfromlockupstream
+UNION ALL
+SELECT
+    chain,
+    'lockupLinear' AS contract,
+    evt_block_time,
+    streamid,
+    asset AS token,
+    amount,
+    'v1.1' AS release_version
+FROM
+    sablier_v2_2_multichain.sablierv2lockuplinear_evt_withdrawfromlockupstream
+UNION ALL
+SELECT
+    chain,
+    'lockupDynamic' AS contract,
+    evt_block_time,
+    streamid,
+    asset AS token,
+    amount,
+    'v1.2' AS release_version
+FROM
+    sablier_v2_2_multichain.sablierv2lockupdynamic_evt_withdrawfromlockupstream
+UNION ALL
+SELECT
+    chain,
+    'lockupTranched' AS contract,
+    evt_block_time,
+    streamid,
+    asset AS token,
+    amount,
+    'v1.2' AS release_version
+FROM
+    sablier_v2_2_multichain.sablierv2lockuptranched_evt_withdrawfromlockupstream
