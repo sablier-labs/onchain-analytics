54d87b58ba13def4e8b53297ca1df35ee2201f78
maxdesalle <max@sablier.finance>
2025-02-03 15:58:29 -0500

    feat: add grouped lockup withdrawal data

---
+++ queries/lockup:_grouped_withdrawal_dat___4611255.sql
@@ -0,0 +1,34 @@
+-- part of a query repo
+-- query name: Lockup: Grouped Withdrawal Data
+-- query link: https://dune.com/queries/4611255
+
+
+WITH
+STREAMAGGREGATION AS (
+    SELECT
+        CHAIN,
+        CONTRACT,
+        STREAMID,
+        RELEASE_VERSION,
+        TOKEN,
+        SUM(AMOUNT) AS TOTAL_AMOUNT,
+        COUNT(*) AS ROW_COUNT
+    FROM
+        QUERY_4611102 -- Lockup: Withdrawal Data
+    GROUP BY
+        CHAIN,
+        RELEASE_VERSION,
+        CONTRACT,
+        STREAMID,
+        TOKEN
+)
+
+SELECT
+    CHAIN,
+    CONTRACT,
+    RELEASE_VERSION,
+    STREAMID,
+    TOKEN,
+    TOTAL_AMOUNT
+FROM
+    STREAMAGGREGATION
