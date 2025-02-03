9896f901e7af039e2486ed68e2401db26dceefea
maxdesalle <max@sablier.finance>
2025-02-03 18:05:03 -0500

    feat: add lockup adjusted remaining balances

---
+++ queries/lockup:_ajusted_remaining_bala___4611374.sql
@@ -0,0 +1,38 @@
+-- part of a query repo
+-- query name: Lockup: Ajusted Remaining Balances
+-- query link: https://dune.com/queries/4611374
+
+
+WITH adjusted_balances AS (
+    SELECT
+        q.chain,
+        q.contract_address,
+        q.contract,
+        q.streamid,
+        q.token,
+        q.deposit,
+        q.withdrawn_amount,
+        q.remaining_balance,
+        t.decimals,
+        q.remaining_balance / POWER(10, t.decimals) AS adjusted_remaining_balance
+    FROM
+        query_4611179 q -- Lockup: Remaining Balances
+    JOIN
+        tokens.erc20
+            t ON q.token = t.contract_address
+    AND q.chain = t.blockchain
+)
+
+SELECT
+    chain,
+    contract_address,
+    contract,
+    streamid,
+    token,
+    deposit,
+    withdrawn_amount,
+    remaining_balance,
+    decimals,
+    adjusted_remaining_balance
+FROM
+    adjusted_balances;
