c9dc4b78523140e7bb592187b86525c31bd30f97
maxdesalle <max@sablier.finance>
2025-02-04 11:32:18 -0500

    feat: add flow most used stablecoins

---
+++ queries/flow:_most_used_stablecoins___4666321.sql
@@ -0,0 +1,24 @@
+-- part of a query repo
+-- query name: Flow: Most Used Stablecoins
+-- query link: https://dune.com/queries/4666321
+
+
+SELECT
+    q.chain,
+    t.symbol,
+    q.token,
+    COUNT(q.token) AS number_of_deposits
+FROM
+    query_4606918 AS q
+LEFT JOIN
+    tokens.erc20 AS t
+    ON
+        q.token = t.contract_address
+        AND q.chain = t.blockchain
+WHERE
+    t.symbol IS NOT NULL
+    AND q.stablecoin_deposit > 0
+GROUP BY
+    q.chain, q.token, t.symbol
+ORDER BY
+    number_of_deposits DESC
