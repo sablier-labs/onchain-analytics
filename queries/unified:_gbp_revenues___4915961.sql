c084320a4faeba50551053fe392322e3ba9cd9dc
maxdesalle <max@sablier.finance>
2025-03-28 11:19:51 -0400

    feat: add gbp revenues

---
+++ queries/unified:_gbp_revenues___4915961.sql
@@ -0,0 +1,24 @@
+-- part of a query repo
+-- query name: Unified: GBP Revenues
+-- query link: https://dune.com/queries/4915961
+
+
+SELECT
+    q.chain,
+    q.currency,
+    q.revenue_date,
+    q.fee,
+    q.usd_asset_price,
+    q.usd_revenue,
+    p.price AS gbp_usd_price,
+    q.usd_asset_price / p.price AS gbp_asset_price,
+    q.usd_revenue / p.price AS gbp_revenue
+FROM
+    query_4907850 q
+LEFT JOIN
+    prices.day p
+    ON
+        DATE(q.revenue_date) = DATE(p.timestamp)
+WHERE
+    p.blockchain = 'gnosis'
+    AND p.contract_address = 0x5cb9073902f2035222b9749f8fb0c9bfe5527108
--- queries/unified:_grouped_revenues___4907742.sql
+++ queries/unified:_grouped_revenues___4907742.sql
@@ -7,7 +7,11 @@ SELECT
     currency,
     revenue_date,
     usd_asset_price,
+    gbp_usd_price,
+    gbp_asset_price,
     SUM(fee) AS fee,
-    SUM(usd_revenue) AS total_usd_revenue
-FROM query_4907850
-GROUP BY currency, revenue_date, usd_asset_price
+    SUM(usd_revenue) AS total_usd_revenue,
+    SUM(gbp_revenue) AS total_gbp_revenue
+FROM query_4915961
+WHERE revenue_date >= DATE_TRUNC('day', CAST('{{Begin Date}}' AS TIMESTAMP)) AND revenue_date <= DATE_TRUNC('day', CAST('{{End Date}}' AS TIMESTAMP))
+GROUP BY currency, revenue_date, usd_asset_price, gbp_usd_price, gbp_asset_price
