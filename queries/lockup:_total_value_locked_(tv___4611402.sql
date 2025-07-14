2fce8cd48c16f563778cfefd05d39ec751b693e1
maxdesalle <max@sablier.finance>
2025-07-14 15:38:42 +0200

    refactor: remove TVL queries

--- queries/lockup:_total_value_locked_(tv___4611402.sql
+++
@@ -1,6 +0,0 @@
--- part of a query repo
--- query name: Lockup: Total Value Locked (TVL)
--- query link: https://dune.com/queries/4611402
-
-
-SELECT SUM(remaining_balance_value) AS tvl FROM query_4611349 -- Lockup: Priced Adjusted Remaining Balances
